594cff3ad07f3f255d8935bc70946515c303a787
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-21 09:53:48 +0200

    minor js changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 //Recupero la parte HTML dove andrà scritto il prezzo calcolato
 const yourPrice = document.getElementById('your_price')
 
+//Salvo il prezzo al km
+const PRICE_PER_KM = 0.21;
+
 //Chiedi il numero di km da percorrere (userKm)
 const userKm = parseFloat(prompt("Quanti km vuoi percorrere?"));
 
@@ -10,23 +13,27 @@ const userAge = parseInt(prompt("Quanti anni hai?"));
 // Variabile globale contenente il prezzo
 let price;
 
-if(!isNaN(userKm) && !isNaN(userAge)) {
+// Variabili globali contenenti lo sconto
+const SALE_FOR_UNDER18 = 0.2;
+const SALE_FOR_OVER65 = 0.4;
+
+if (!isNaN(userKm) && !isNaN(userAge)) {
     console.log("km e età: ", userKm, userAge);
 
     //Calcola il prezzo del biglietto:
     //Prezzo = 0.21€ x km
-    price = 0.21 * userKm;
+    price = PRICE_PER_KM * userKm;
     console.log("prezzo senza sconti: ", price);
 
     //SE l'utente è minorenne (userAge < 18anni) applica uno sconto del 20%
-    if(userAge < 18) {
-        price -= price * 0.2;
+    if (userAge < 18) {
+        price -= price * SALE_FOR_UNDER18;
         console.log("prezzo se minorenne: ", price);
     }
 
     //SE l'utente ha più di 65 anni (userAge > 65anni) applica uno sconto del 40%
-    if(userAge > 65) {
-        price -= price * 0.4;
+    if (userAge > 65) {
+        price -= price * SALE_FOR_OVER65;
         console.log("prezzo se over 65: ", price);
     }
 
